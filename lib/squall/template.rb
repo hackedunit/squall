027968659242c84dbb4a4b98803c411c559ae4af
Tinu Cleatus <tinu.cleatus@me.com>
2012-01-04 15:53:17 +0530

    Show and Delete for template

--- lib/squall/template.rb
+++ lib/squall/template.rb
@@ -6,11 +6,23 @@ module Squall
       response = request(:get, '/templates.json')
       response.collect { |temp| temp['image_template'] }
     end
+    
+    # Return a Hash for the given Template
+    def show(id)
+      response = request(:get, "/templates/#{id}.json")
+      response.first[1]
+    end
 
     # Make a Template public so that it can be downloaded
     # via a HTTP url
     def make_public(id)
       response = request(:post, "/templates/#{id}/make_public.json")
+      response.first[1]
+    end
+    
+    # Delete a template
+    def delete(id)
+      response = request(:delete, "/templates/#{id}.json")
       response.first[1]
     end
   end
